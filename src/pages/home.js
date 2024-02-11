d8ab6b077fe5b241a595312a9511f3acf6f8a955
anamkaa <75376715+anamkaa@users.noreply.github.com>
2024-02-11 15:45:15 +0530

    fixed css

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,7 +13,7 @@ const Homepage = () => {
         <p className="text-center">Hello👋 , I am Anamika</p>
         <h1 class="hero-heading">Front-End React Developer</h1>
         <div class="container container-center text-center">
-          <div class="list list-hr">
+          <div class="list-align">
             <span>Tech Stack:</span>
             <span className="link link-primary mrg">html</span>
             <span className="link link-primary mrg">css</span>
