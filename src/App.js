21fda5ee3ca8a1e4d4b7f963e9fef2e0d617ffdd
anamkaa <75376715+anamkaa@users.noreply.github.com>
2024-02-11 13:41:55 +0530

    refactored code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import "./App.css";
-import hero from "./images/hero.svg";
-import Projects from "./pages/projects";
 import Basicprojects from "./pages/basicprojects";
 import Blogs from "./pages/blogs";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Homepage from "./pages/home";
+import Projects from "./pages/projects";
 
 function App() {
   return (
     <div className="App">
-
-        <Navbar/>
-
-        <header class="hero">
-          <img class="hero-img image-round" src={hero} alt="image" />
-          <p className="text-center">Hello👋 , I am Anamika</p>
-          <h1 class="hero-heading">Front-End React Developer</h1>
-          <div class="container container-center text-center">
-            <div class="list list-hr">
-              <span>Tech Stack:</span>
-              <span className="link link-primary mrg">html</span>
-              <span className="link link-primary mrg">css</span>
-              <span className="link link-primary mrg">javascript</span>
-              <span className="link link-primary mrg">reactjs</span>
-              <span className="link link-primary mrg">nodejs</span>
-              <span className="link link-primary mrg">git</span>
-            </div>
-          </div>
-        </header>
-
-        <Footer/>
+      <Homepage/>
       <Projects/>
       <Basicprojects/>
       <Blogs/>
---
+++ src/pages/home.js
@@ -0,0 +1,33 @@
+import "../../src/App.css";
+import hero from "../images/hero.svg";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Homepage = () => {
+    return (
+        <>
+         <Navbar/>
+
+<header class="hero">
+  <img class="hero-img image-round" src={hero} alt="image" />
+  <p className="text-center">Hello👋 , I am Anamika</p>
+  <h1 class="hero-heading">Front-End React Developer</h1>
+  <div class="container container-center text-center">
+    <div class="list list-hr">
+      <span>Tech Stack:</span>
+      <span className="link link-primary mrg">html</span>
+      <span className="link link-primary mrg">css</span>
+      <span className="link link-primary mrg">javascript</span>
+      <span className="link link-primary mrg">reactjs</span>
+      <span className="link link-primary mrg">nodejs</span>
+      <span className="link link-primary mrg">git</span>
+    </div>
+  </div>
+</header>
+
+<Footer/>
+        </>
+    )
+}
+
+export default Homepage;
